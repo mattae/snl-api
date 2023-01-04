69999007d497a4cff17c82e4509f12087c40a2e1
jbellatech <111922398+jbellatech@users.noreply.github.com>
2023-01-05 00:22:20 +0100

    Revert "- Enhance value set service to be able to retrieve value or display t…"

--- src/main/java/io/github/jbella/snl/core/api/services/ValueSetService.java
+++ src/main/java/io/github/jbella/snl/core/api/services/ValueSetService.java
@@ -1,17 +1,11 @@
 package io.github.jbella.snl.core.api.services;
 
-import io.github.jbella.snl.core.api.domain.ValueSet;
-
 import java.util.List;
 
 import static io.github.jbella.snl.core.api.domain.ValueSet.BaseView;
 
-public interface ValueSetService {
+public interface ValueSetService  {
     List<BaseView> getValuesForSystem(String system, String lang, Boolean active);
 
     String getDisplay(String system, String code, String lang);
-
-    List<ValueSet.BaseView> getValuesByCategoryKey(String category, String key, String lang, Boolean active);
-
-    String getDisplayByCategoryKey(String category, String key, String code, String lang);
 }
