f97482efc4a8c3dcd3c65bb89977cb4270430a91
Matthew Edor <matthew.edor@gmail.com>
2023-11-21 21:22:51 +0100

    - Add icon set support

---
+++ src/main/java/io/github/jbella/snl/core/api/domain/IconSet.java
@@ -0,0 +1,35 @@
+package io.github.jbella.snl.core.api.domain;
+
+import com.blazebit.persistence.view.EntityView;
+import io.github.jbella.snl.core.api.id.UUIDV7;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "fw_icon_set")
+public class IconSet {
+    @Id
+    @UUIDV7
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private String namespace;
+
+    private String svg;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Plugin plugin;
+
+    @EntityView(IconSet.class)
+    public interface View {
+        String getNamespace();
+
+        String getSvg();
+    }
+}
