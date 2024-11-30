0978c99deac4de2f99e524dff5d41450de491bb7
mattae <matthew.edor@gmail.com>
2024-11-30 16:05:55 +0100

    - Update preference setup

--- src/main/java/io/github/mattae/snl/core/api/services/PreferenceService.java
+++ src/main/java/io/github/mattae/snl/core/api/services/PreferenceService.java
@@ -1,38 +1,24 @@
 package io.github.mattae.snl.core.api.services;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import io.github.mattae.snl.core.api.domain.Preference;
 import jakarta.annotation.Nonnull;
-import jakarta.annotation.Nullable;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.transaction.TransactionSystemException;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 public interface PreferenceService {
-    /**
-     * Performs a Preference save operation.
-     *
-     * @param preference the Preference value to save
-     * @return the saved Preference
-     * @throws DataIntegrityViolationException if the key in a category is repeated or the category is repeated without keys
-     * @throws TransactionSystemException if the category or data is not provided
-     */
 
     Preference save(Preference preference);
 
     void delete(Preference preference);
 
-    Optional<Preference> getPreference(@Nonnull String category, @Nullable String key);
+    Optional<Preference> getPreference(@Nonnull String category);
 
-    Optional<Double> getNumericValue(@Nonnull String category, @Nullable String key);
+    Optional<Double> getNumericValueForKey(@Nonnull String category, @Nonnull String key);
 
-    Optional<String> getStringValue(@Nonnull String category, @Nullable String key);
+    Optional<String> getStringValueForKey(@Nonnull String category, @Nonnull String key);
 
-    Optional<Boolean> getBoolValue(@Nonnull String category, @Nullable String key);
+    Optional<Boolean> getBoolValueForKey(@Nonnull String category, @Nonnull String key);
 
-    Optional<LocalDate> getDateValue(@Nonnull String category, @Nullable String key);
-
-    Optional<JsonNode> getJsonValue(@Nonnull String category, @Nullable String key);
+    Optional<LocalDate> getDateValueForKey(@Nonnull String category, @Nonnull String key);
 }
